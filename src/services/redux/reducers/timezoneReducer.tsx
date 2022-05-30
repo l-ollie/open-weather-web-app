8786df19c4cccc8bb6f68f6ba8b4e1dbc7bb1e3e
l-ollie <ollieweijermars@gmail.com>
2022-05-30 17:16:41 +0200

    removed X from inputfield

--- src/services/redux/reducers/timezoneReducer.tsx
+++ src/services/redux/reducers/timezoneReducer.tsx
@@ -1,8 +1,6 @@
 import ActionType from '../actionTypes';
 import { TimezoneR } from '../types';
 
-
-
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state: null | string = null, action: TimezoneR) => {
 	switch (action.type) {
